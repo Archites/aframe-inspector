1b7ed825389dc951c0520b83279bb4347e39c2a6
narongsakjob <narongsak.chobsri@gmail.com>
2019-03-03 21:55:41 +0700

    Update script

--- .circleci/firebase-script.js
+++ .circleci/firebase-script.js
@@ -1,4 +1,8 @@
 var firebase = require('firebase')
+var fs = require('fs')
+require('firebase/storage')
+global.XMLHttpRequest = require("xhr2")
+
 var config = {
   apiKey: "AIzaSyA8_QEUXbgz3qZTAQkYldpMNBuVd7uv3-Y",
   authDomain: "vr-chitech.firebaseapp.com",
@@ -9,3 +13,16 @@ var config = {
 }
 
 firebase.initializeApp(config)
+var storageRef = firebase.storage().ref()
+
+fs.readFile('./dist/aframe-inspector.js', function(err, data) {
+  if (err) throw err
+  var metadata = {
+    contentType: 'text/javascript',
+  };
+
+  storageRef.child('aframe-inspector.js').put(data, metadata).then(function(snapshot) {
+    console.log(snapshot)
+    process.exit(0)
+  })
+})
