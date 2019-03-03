763f55e5ae11d8213feb295dce93ab8fcf8f4575
narongsakjob <narongsak.chobsri@gmail.com>
2019-03-03 20:38:24 +0700

    Initial firebase

--- .circleci/firebase-script.js
+++ .circleci/firebase-script.js
@@ -1,1 +1,11 @@
-console.log('eiei')
+var firebase = require('firebase')
+var config = {
+  apiKey: "AIzaSyA8_QEUXbgz3qZTAQkYldpMNBuVd7uv3-Y",
+  authDomain: "vr-chitech.firebaseapp.com",
+  databaseURL: "https://vr-chitech.firebaseio.com",
+  projectId: "vr-chitech",
+  storageBucket: "vr-chitech.appspot.com",
+  messagingSenderId: "294689746221"
+}
+
+firebase.initializeApp(config)
