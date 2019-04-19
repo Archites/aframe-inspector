e460ffbaf670e61f356ca37eea8ed77bf1745377
narongsakjob <narongsak.chobsri@gmail.com>
2019-04-20 01:01:30 +0700

    Ignore firebase config

--- .circleci/firebase-script.js
+++ .circleci/firebase-script.js
@@ -4,20 +4,18 @@ require('firebase/storage')
 global.XMLHttpRequest = require("xhr2")
 
 var config = process.argv[2]
-console.log(process.argv)
 
+firebase.initializeApp(config)
+var storageRef = firebase.storage().ref()
 
-// firebase.initializeApp(config)
-// var storageRef = firebase.storage().ref()
+fs.readFile('./dist/aframe-inspector.js', function(err, data) {
+  if (err) throw err
+  var metadata = {
+    contentType: 'text/javascript',
+  };
 
-// fs.readFile('./dist/aframe-inspector.js', function(err, data) {
-//   if (err) throw err
-//   var metadata = {
-//     contentType: 'text/javascript',
-//   };
-
-//   storageRef.child('aframe-inspector.js').put(data, metadata).then(function(snapshot) {
-//     console.log(snapshot)
-//     process.exit(0)
-//   })
-// })
+  storageRef.child('aframe-inspector.js').put(data, metadata).then(function(snapshot) {
+    console.log(snapshot)
+    process.exit(0)
+  })
+})
