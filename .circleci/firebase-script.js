e03a8a8bd0979c237bd3d1e090c8c137651ef20b
narongsakjob <narongsak.chobsri@gmail.com>
2019-04-20 01:18:56 +0700

    Ignore firebase config

--- .circleci/firebase-script.js
+++ .circleci/firebase-script.js
@@ -8,8 +8,9 @@ console.log(process.argv[2])
 console.log(process.argv[2].toString())
 var config = process.argv[2]
 console.log(config)
+var x = {apiKey:"AIzaSyA8_QEUXbgz3qZTAQkYldpMNBuVd7uv3-Y",authDomain:"vr-chitech.firebaseapp.com",databaseURL:"https://vr-chitech.firebaseio.com",projectId:"vr-chitech",storageBucket:"vr-chitech.appspot.com",messagingSenderId:"294689746221"}
 
-firebase.initializeApp(config)
+firebase.initializeApp(x)
 var storageRef = firebase.storage().ref()
 
 fs.readFile('./dist/aframe-inspector.js', function(err, data) {
@@ -23,3 +24,4 @@ fs.readFile('./dist/aframe-inspector.js', function(err, data) {
     process.exit(0)
   })
 })
+
